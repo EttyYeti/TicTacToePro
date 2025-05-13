aa67138cf4c9103c6b77749358c82646fb0948cb
EttyYeti <dromo2327@gmail.com>
2025-05-13 11:40:20 -0500

    script_update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,94 @@ public class GameManager : MonoBehaviour
 {
     public bool isPlayerXTurn = true;
     public List<Button> tileButtons;
+    public TextMeshProUGUI winMessageText;
+    private string[] board = new string[9];
 
     void Start()
     {
-        foreach (Button btn in tileButtons)
+         for (int i = 0; i < tileButtons.Count; i++)
         {
-            btn.onClick.AddListener(() => OnTileClicked(btn));
+            int index = i; // capture index for closure
+            tileButtons[i].onClick.AddListener(() => OnTileClicked(index));
         }
     }
 
-    void OnTileClicked(Button clickedButton)
+    void OnTileClicked(int index)
     {
-        // Get the TextMeshPro component inside the button
-        TextMeshProUGUI tmp = clickedButton.GetComponentInChildren<TextMeshProUGUI>();
+        if (board[index] != null)
+            return;
+
+        string currentPlayer = isPlayerXTurn ? "X" : "O";
+        board[index] = currentPlayer;
+
+        TextMeshProUGUI tmp = tileButtons[index].GetComponentInChildren<TextMeshProUGUI>();
         if (tmp != null)
         {
-            tmp.text = isPlayerXTurn ? "X" : "O";
+            tmp.text = currentPlayer;
         }
 
-        clickedButton.interactable = false; // Disable button
-        isPlayerXTurn = !isPlayerXTurn;     // Switch turns
+        tileButtons[index].interactable = false;
+        if (CheckWin(currentPlayer))
+        {
+            Debug.Log(currentPlayer + " wins!");
+            EndGame();
+        }
+        else if (CheckDraw())
+        {
+            Debug.Log("Draw!");
+            EndGame();
+        }
+        else
+        {
+            isPlayerXTurn = !isPlayerXTurn;
+        }
+    }
 
-        // (Later) Call win/draw check here
+    bool CheckWin(string player)
+    {
+        int[,] winPatterns = new int[,]
+        {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // columns
+            {0, 4, 8}, {2, 4, 6}             // diagonals
+        };
+
+        for (int i = 0; i < winPatterns.GetLength(0); i++)
+        {
+            if (board[winPatterns[i, 0]] == player &&
+                board[winPatterns[i, 1]] == player &&
+                board[winPatterns[i, 2]] == player)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool CheckDraw()
+    {
+        foreach (string val in board)
+        {
+            if (string.IsNullOrEmpty(val))
+                return false;
+        }
+        return true;
+    }
+
+    void EndGame()
+    {
+        // Disable all buttons
+        foreach (var btn in tileButtons)
+        {
+            btn.interactable = false;
+        }
+
+        winMessageText.gameObject.SetActive(true);
+
+        if (CheckDraw())
+            winMessageText.text = "It's a Draw!";
+        else
+            winMessageText.text = (isPlayerXTurn ? "X" : "O") + " Wins!";
     }
 }
